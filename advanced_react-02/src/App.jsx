50510dd6d39b797cb7fa66a951fe1435529f9d20
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-01-30 15:54:43 +0530

    Project_structure

--- advanced_react-02/src/App.jsx
+++ advanced_react-02/src/App.jsx
@@ -1,11 +1,22 @@
 import Starter from './tutorial/02-useEffect/starter/05-cleanup-function'
 import Final from './tutorial/03-conditional-rendering/final/05-short-circuit-examples'
+import Example from './tutorial/04-project-structure/starter/Examples'
+import Navbar from './tutorial/04-project-structure/starter/Navbar'
+import { Home, About } from './tutorial/04-project-structure/starter/Pages'
+// import About from './tutorial/04-project-structure/starter/Pages/About'
 
 function App() {
   return (
     <div className="container">
-      <Starter />
+      {/* <Starter /> */}
       {/* <Final /> */}
+      {/* <Navbar /> */}
+      {/* <Home />
+      <About /> */}
+      <Example />
+      <div>
+        <h2>Glean</h2>
+      </div>
     </div>
   )
 }
---
+++ advanced_react-02/src/tutorial/04-project-structure/starter/Examples/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FirstComponent from './FirstComponent'
+import SecondComponent from './SecondComponent'
+
+const Example = () => {
+  return (
+    <>
+      <FirstComponent />
+      <SecondComponent />
+    </>
+  )
+}
+
+export default Example
