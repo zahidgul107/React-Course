25ce35a34fe3ae12098a164cd8dfb0dee0655d85
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-01-30 12:37:38 +0530

    conditional rendering

--- advanced_react-02/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ advanced_react-02/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,32 @@
-const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
-};
+import { useEffect, useState } from 'react'
 
-export default CleanupFunction;
+const CleanupFunction = () => {
+  const [toggle, setToggle] = useState(false)
+  console.log('render')
+  return (
+    <>
+      <button className="btn" onClick={() => setToggle(!toggle)}>
+        Toggle Component
+      </button>
+      {toggle && <RandomComponent />}
+    </>
+  )
+}
+
+const RandomComponent = () => {
+  useEffect(() => {
+    // console.log('hmm, this is interesting')
+    // const intId = setInterval(() => {
+    //   console.log('Hello from interval')
+    // }, 1000)
+    // return () => {
+    //   clearInterval(intId)
+    // }
+    const someFunc = () => {}
+    window.addEventListener('scroll', someFunc)
+    return window.removeEventListener('scroll', someFunc)
+  }, [])
+  return <div>Hello there</div>
+}
+
+export default CleanupFunction
