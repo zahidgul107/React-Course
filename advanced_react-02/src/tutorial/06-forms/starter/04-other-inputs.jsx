905a40a67766e8269c60bd2299c8534d48f17aab
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-02-07 20:04:40 +0530

    implemention of formData

--- advanced_react-02/src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ advanced_react-02/src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 const frameworks = ['react', 'angular', 'vue', 'svelte']
 const OtherInputs = () => {
   const [shipping, setShipping] = useState(false)
-  const [framework, setFramework] = useState(false)
+  const [framework, setFramework] = useState('react')
 
   const handleShipping = (e) => {
     console.log(e.target.checked)
     setShipping(e.target.checked)
+  }
+
+  const handleFramework = (e) => {
+    console.log(e.target.value)
+    setFramework(e.target.value)
   }
   return (
     <div>
@@ -27,6 +32,16 @@ const OtherInputs = () => {
           <label htmlFor="framework" className="form-label">
             Framework
           </label>
+          <select
+            name="framework"
+            id="framework"
+            value={framework}
+            onChange={handleFramework}
+          >
+            {frameworks.map((framework) => {
+              return <option key={framework}>{framework}</option>
+            })}
+          </select>
         </div>
         <button type="submit" className="btn btn-block">
           submit
