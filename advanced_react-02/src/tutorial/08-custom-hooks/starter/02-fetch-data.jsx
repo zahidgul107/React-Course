1f4d828be1cb10f99dbfa18ce0af662ac4fd5658
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-02-08 10:50:32 +0530

    custom Hooks

--- advanced_react-02/src/tutorial/08-custom-hooks/starter/02-fetch-data.jsx
+++ advanced_react-02/src/tutorial/08-custom-hooks/starter/02-fetch-data.jsx
@@ -1,43 +1,46 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+//import { useEffect, useState } from 'react'
+import useFetch from './useFetch'
+import useFetchPerson from './useFetchPerson'
+const url = 'https://api.github.com/users/QuincyLarson'
 
 const FetchData = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-  const [user, setUser] = useState(null);
+  const { isLoading, isError, data: user } = useFetch(url)
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [isError, setIsError] = useState(false);
+  // const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const resp = await fetch(url);
-        // console.log(resp);
-        if (!resp.ok) {
-          setIsError(true);
-          setIsLoading(false);
-          return;
-        }
+  // useEffect(() => {
+  //   const fetchUser = async () => {
+  //     try {
+  //       const resp = await fetch(url);
+  //       // console.log(resp);
+  //       if (!resp.ok) {
+  //         setIsError(true);
+  //         setIsLoading(false);
+  //         return;
+  //       }
 
-        const user = await resp.json();
-        setUser(user);
-      } catch (error) {
-        setIsError(true);
-        // console.log(error);
-      }
-      // hide loading
-      setIsLoading(false);
-    };
-    fetchUser();
-  }, []);
+  //       const user = await resp.json();
+  //       setUser(user);
+  //     } catch (error) {
+  //       setIsError(true);
+  //       // console.log(error);
+  //     }
+  //     // hide loading
+  //     setIsLoading(false);
+  //   };
+  //   fetchUser();
+  // }, []);
   // order matters
   // don't place user JSX before loading or error
 
   if (isLoading) {
-    return <h2>Loading...</h2>;
+    return <h2>Loading...</h2>
   }
   if (isError) {
-    return <h2>There was an error...</h2>;
+    return <h2>There was an error...</h2>
   }
-  const { avatar_url, name, company, bio } = user;
+  const { avatar_url, name, company, bio } = user
   return (
     <div>
       <img
@@ -49,6 +52,6 @@ const FetchData = () => {
       <h4>works at {company}</h4>
       <p>{bio}</p>
     </div>
-  );
-};
-export default FetchData;
+  )
+}
+export default FetchData
