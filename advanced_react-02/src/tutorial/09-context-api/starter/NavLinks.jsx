576620fe86fab18e17da052b15d167a291c580d6
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-02-12 18:13:28 +0530

    context api

---
+++ advanced_react-02/src/tutorial/09-context-api/starter/NavLinks.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import UserContainer from './UserContainer'
+
+const NavLinks = () => {
+  return (
+    <div className="nav-container">
+      <ul className="nav-links">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+      </ul>
+      <UserContainer />
+    </div>
+  )
+}
+
+export default NavLinks
