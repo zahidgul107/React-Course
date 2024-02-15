347124a89c94567465b0d28049f002a86f4f6d6b
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-02-15 23:52:05 +0530

    using axios

--- redux-toolkit/src/feature/cart/cartSlice.js
+++ redux-toolkit/src/feature/cart/cartSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import cartItems from '../../cartItems'
+import axios from 'axios'
+import { openModal } from '../modal/modalSlice'
 
 const url = 'https://course-api.com/react-useReducer-cart-project'
 
@@ -10,11 +11,22 @@ const initialState = {
   isLoading: true,
 }
 
-export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
-  return fetch(url)
-    .then((resp) => resp.json())
-    .catch((err) => console.log(error))
-})
+export const getCartItems = createAsyncThunk(
+  'cart/getCartItems',
+  async (name, thunkAPI) => {
+    try {
+      // console.log(name);
+      // console.log(thunkAPI);
+      // console.log(thunkAPI.getState());
+      // thunkAPI.dispatch(openModal());
+      const resp = await axios(url)
+
+      return resp.data
+    } catch (error) {
+      return thunkAPI.rejectWithValue('something went wrong')
+    }
+  }
+)
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -24,16 +36,15 @@ const cartSlice = createSlice({
       state.cartItems = []
     },
     removeItem: (state, action) => {
-      console.log(action)
       const itemId = action.payload
       state.cartItems = state.cartItems.filter((item) => item.id !== itemId)
     },
     increase: (state, { payload }) => {
-      const cartItem = state.cartItems.find((item) => item.id == payload.id)
+      const cartItem = state.cartItems.find((item) => item.id === payload.id)
       cartItem.amount = cartItem.amount + 1
     },
     decrease: (state, { payload }) => {
-      const cartItem = state.cartItems.find((item) => item.id == payload.id)
+      const cartItem = state.cartItems.find((item) => item.id === payload.id)
       cartItem.amount = cartItem.amount - 1
     },
     calculateTotals: (state) => {
@@ -46,24 +57,25 @@ const cartSlice = createSlice({
       state.amount = amount
       state.total = total
     },
-    extraReducers: {
-      [getCartItems.pending]: (state) => {
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCartItems.pending, (state) => {
         state.isLoading = true
-      },
-      [getCartItems.fulfilled]: (state, action) => {
-        console.log(action)
+      })
+      .addCase(getCartItems.fulfilled, (state, action) => {
+        // console.log(action);
         state.isLoading = false
         state.cartItems = action.payload
-      },
-      [getCartItems.rejected]: (state) => {
+      })
+      .addCase(getCartItems.rejected, (state, action) => {
+        console.log(action)
         state.isLoading = false
-      },
-    },
+      })
   },
 })
 
-//console.log(cartSlice)
-
+// console.log(cartSlice);
 export const { clearCart, removeItem, increase, decrease, calculateTotals } =
   cartSlice.actions
 
