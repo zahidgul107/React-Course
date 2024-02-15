adbd48f080d15685ad37d6b7a5df68bc3bca86c2
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2024-02-15 23:31:30 +0530

    fetching data from external library

--- redux-toolkit/src/App.jsx
+++ redux-toolkit/src/App.jsx
@@ -1,21 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
 import CartContainer from './components/CartContainer'
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { calculateTotals } from './feature/cart/cartSlice'
+import { calculateTotals, getCartItems } from './feature/cart/cartSlice'
 import Modal from './components/Modal'
 
 function App() {
-  const { cartItems } = useSelector((store) => store.cart)
+  const { cartItems, isLoading } = useSelector((state) => state.cart)
   const { isOpen } = useSelector((store) => store.modal)
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(calculateTotals())
   }, [cartItems])
+
+  useEffect(() => {
+    dispatch(getCartItems())
+  }, [])
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
   return (
     <>
       {isOpen && <Modal />}
